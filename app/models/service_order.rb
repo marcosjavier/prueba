ebd9cb49201b67e33310559375f2eae641433d0f
marcosjavier <javierojeda21@gmail.com>
2020-07-26 17:22:27 -0300

    pagina inicio muestra totales pending_so y pending_maintenances

--- app/models/service_order.rb
+++ app/models/service_order.rb
@@ -1,9 +1,10 @@
 class ServiceOrder < ActiveRecord::Base
-  scope :pending, -> { where(status_id: 1)}
+	scope :pending, -> { where(status_id: 1)}
 	scope :created_between, ->(date_start,date_end) { where(created_at: date_start..date_end) }
 	scope :opened, -> { where("status_id = 1")}
 	scope :closed, -> { where("status_id = 2")}
-  before_create :set_fields
+
+	before_create :set_fields
 	belongs_to :customer
 	belongs_to :status
 	has_one :device
@@ -12,12 +13,10 @@ class ServiceOrder < ActiveRecord::Base
 	accepts_nested_attributes_for :payments
 	accepts_nested_attributes_for :movements, reject_if: :all_blank, allow_destroy: true
 	monetize :cost_cents
-	
-	
 
 	validates :customer_id, presence: { message: ' El campo del Cliente no puede estar en vacio' }
-	
-	
+		
+		
 	def set_fields
 		creation_date_at = Date.today
 
@@ -25,22 +24,22 @@ class ServiceOrder < ActiveRecord::Base
 
 	def get_last_date_of_movement
 		if movements.present?
-			movements.last.updated_at			
-		end	 
-		
+				movements.last.updated_at           
+		end  
+			
 	end
 
 	def get_status
 		self.status.description
-		
+			
 	end
 
-	
+		
 
-  #def self.filter(date_start,date_end)
-  #
-  #  service_orders = ServiceOrder.where(created_at: date_start..date_end)
+	#def self.filter(date_start,date_end)
+	#
+	#  service_orders = ServiceOrder.where(created_at: date_start..date_end)
 
-  #end
+	#end
 
 end
